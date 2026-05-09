d50b3d035358c818cdf4db596c482d4b1f72409a
Parth Illendula <parthos623@gmail.com>
2026-05-09 13:58:02 -0500

    Remove debug log from storageService

--- src/services/storageService.js
+++ src/services/storageService.js
@@ -26,7 +26,6 @@ export const saveGame = async (game) => {
     };
     games.push(newGame);
     await AsyncStorage.setItem(STORAGE_KEYS.GAMES, JSON.stringify(games));
-    console.log('Saved game:', newGame.id, newGame);
     return newGame;
   } catch (error) {
     console.error('Error saving game:', error);
